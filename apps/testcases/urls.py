4667cbf8aa46c9c70a099042c16439f381939b1e
Hiền Kim <hiencaok@gmail.com>
2026-08-07 16:18:57 +0700

    feat(requirement): implement requirement CRUD API

--- apps/testcases/urls.py
+++ apps/testcases/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from apps.testcases.api_views import ProjectViewSet
+from apps.testcases.api_views import RequirementViewSet
 
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet, basename='project')
+router.register(r'requirements', RequirementViewSet, basename='requirement')
 
 urlpatterns = router.urls
